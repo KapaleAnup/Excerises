46c9883d72da9304fc1fa164ac090b8e23fe319f
Anup Kapale <anup.k@turtlemint.com>
2020-05-08 12:21:26 +0530

    added some practice quetions

---
+++ src/main/java/javaInterviewQuestions/AcciiChar.java
@@ -0,0 +1,16 @@
+package javaInterviewQuestions;
+
+public class AcciiChar {
+
+    public static void main(String[] args) {
+        char  c ='0';
+        int accsii = c;
+
+        int acciiNumber = (int) c;
+
+        System.out.println(accsii);
+        System.out.println(acciiNumber);
+    }
+
+
+}
---
+++ src/main/java/javainterviewquiestionsset/PrintInteger.java
@@ -0,0 +1,16 @@
+package javainterviewquiestionsset;
+
+import java.util.Scanner;
+
+public class PrintInteger {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the number");
+        int number = scanner.nextInt();
+
+        System.out.println("You entered number is: " + number);
+    }
+}
